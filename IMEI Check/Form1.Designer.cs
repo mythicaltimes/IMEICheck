74b052cead14ec319a400b3af3c7b3c05535a9ac
Bradley Mruzek <Bradley Mruzek>
2017-04-27 11:28:00 -0400

    Initial Upload

---
+++ IMEI Check/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace IMEI_Check
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.importCSVbutton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(128, 12);
+            this.textBox1.MaxLength = 14;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IMEI Number:";
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxResult.Location = new System.Drawing.Point(128, 38);
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.ReadOnly = true;
+            this.textBoxResult.Size = new System.Drawing.Size(150, 20);
+            this.textBoxResult.TabIndex = 16;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(65, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 20);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "Result:";
+            // 
+            // importCSVbutton
+            // 
+            this.importCSVbutton.Location = new System.Drawing.Point(2, 33);
+            this.importCSVbutton.Name = "importCSVbutton";
+            this.importCSVbutton.Size = new System.Drawing.Size(58, 38);
+            this.importCSVbutton.TabIndex = 19;
+            this.importCSVbutton.Text = "Import CSV";
+            this.importCSVbutton.UseVisualStyleBackColor = true;
+            this.importCSVbutton.Click += new System.EventHandler(this.importCSVbutton_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 83);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(276, 199);
+            this.dataGridView1.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 274);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.importCSVbutton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(300, 313);
+            this.MinimumSize = new System.Drawing.Size(300, 313);
+            this.Name = "Form1";
+            this.Text = "IMEI Check";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button importCSVbutton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ IMEI Check/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace IMEI_Check
+{
+    public partial class Form1 : Form
+    {
+        public static int last;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+        private static string GetLuhnCheckDigit(string number)
+        {
+            var sum = 0;
+            var alt = true;
+            var digits = number.ToCharArray();
+            for (int i = digits.Length - 1; i >= 0; i--)
+            {
+                var curDigit = (digits[i] - 48);
+                if (alt)
+                {
+                    curDigit *= 2;
+                    if (curDigit > 9)
+                        curDigit -= 9;
+                }
+                sum += curDigit;
+                alt = !alt;
+            }
+            if ((sum % 10) == 0)
+            {
+                last = 0;
+                return "0";
+            }
+            int temp = sum % 10;
+            temp = temp - 10;
+            temp = temp * -1;
+            last = temp;
+            return (10 - (sum % 10)).ToString();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            GetLuhnCheckDigit(textBox1.Text);
+            if (textBox1.Text.Length == 14)
+            {
+                Clipboard.SetText(textBox1.Text + last.ToString());
+            }
+            textBoxResult.Text = textBox1.Text + last.ToString();
+        }
+
+        private void importCSVbutton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Dialog = new OpenFileDialog();
+            Dialog.Filter = "CSV Files | *.csv";
+            Dialog.Multiselect = false;
+            string path = Dialog.FileName;
+
+            DataTable dt1 = new DataTable();
+            dt1.Columns.Add("Original");
+            dt1.Columns.Add("New");
+
+
+            if (Dialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (var fs = File.OpenRead(Dialog.FileName))
+                    {
+                        using (var reader = new StreamReader(fs))
+                        {
+
+                            List<string> lista = new List<string>();
+                            List<string> listb = new List<string>();
+                            string temp;
+
+                            while (!reader.EndOfStream)
+                            {
+                                var line = reader.ReadLine();
+                                var values = line.Split(',');
+                                lista.Add(values[0]);
+                                GetLuhnCheckDigit(values[0]);
+                                listb.Add(last.ToString());
+                                temp = values[0] + last.ToString();
+                                dt1.Rows.Add(values[0], temp);
+                            }
+                            dataGridView1.DataSource = dt1;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+    }
+}
